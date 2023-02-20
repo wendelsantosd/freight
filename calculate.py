dc7fba039dccc6973ffbbac645acda3fe7268171
wendelsantosd <wendelwcsantos@gmail.com>
2023-02-20 17:56:42 -0300

    refactor: calculate

--- calculate.py
+++ calculate.py
@@ -1,12 +1,17 @@
-km = float(input('Digite o KM a ser percorrido: '))
+km1 = float(input('Digite o KM de IDA a ser percorrido: '))
+km2 = float(input('Digite o KM de VOLTA ser percorrido: '))
 
+kmTotal = km1 + km2
 currentValueGasoline = 5.5
 
-gasolineInLiterPerKm = km / 16
+gasolineInLiterPerKm1 = km1 / 16
+gasolineInLiterPerKm2 = km2 / 16
 
-priceGasoline = gasolineInLiterPerKm * currentValueGasoline * 2
+gasolineInLiterPerKmTotal = gasolineInLiterPerKm1 + gasolineInLiterPerKm1
 
-print(f'Kilometragem total a ser percorrida: {km:.2f} KM')
+priceGasoline = gasolineInLiterPerKmTotal * currentValueGasoline
+
+print(f'Kilometragem total a ser percorrida: {kmTotal:.2f} KM')
 print(f'Valor atual da gasolina: R$ {currentValueGasoline:.2f}')
-print(f'Litros gastos em gasolina: {gasolineInLiterPerKm:.2f} L')
+print(f'Litros gastos em gasolina: {gasolineInLiterPerKmTotal:.2f} L')
 print(f'Valor a ser cobrado pelo frete: R$ {priceGasoline:.2f}')
